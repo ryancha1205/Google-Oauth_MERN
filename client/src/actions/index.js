67ffb4ff4c9a6c08ded858a2285fd007030e9b6a
herkas413 <herkas413@naver.com>
2017-09-20 17:07:12 +0800

    Added billing and client app

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -18,3 +18,11 @@ export const fetchUser = ()=>{
 //   const res = axios.get('/api/current_user');
 //   dispatch({ type:FETCH_USER, payload: res });
 // };
+
+
+export const handleToken =(token) => async dispatch =>{
+  const res = await axios.post('/api/stripe',token);
+  
+  dispatch({type:FETCH_USER,payload: res.data});
+
+};
--- index.js
+++ index.js
@@ -12,18 +12,31 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //req.body를 쓸수있게 해준다.
 app.use(
   cookieSession({
     maxAge: 30*24*60*60*1000,
     keys: [keys.cookieKey]
   })
 );
-
 app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+//to make sure production environment
+if(process.env.NODE_ENV==="production"){
+  //Express will serve up production assets
+  // like out main.js file, or main.css file!
+  app.use(express.statc('client/build'));
+  //Express will serve up the index.html file
+  //if it doesn't recognize the route
+  const path =require('path');
+  app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+  });
+}
 
 
 const PORT = process.env.PORT||5000;
