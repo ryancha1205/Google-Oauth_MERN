c2b97ebea417f9b18391984236769077ebcda3d2
herkas413 <herkas413@naver.com>
2017-09-20 17:28:26 +0800

    reset

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ require('./routes/billingRoutes')(app);
 if(process.env.NODE_ENV==="production"){
   //Express will serve up production assets
   // like out main.js file, or main.css file!
-  app.use(express.statc('client/build'));
+  app.use(express.static('client/build'));
   //Express will serve up the index.html file
   //if it doesn't recognize the route
   const path =require('path');
